6735e8a8a98b5148b27f4e94a58a7b9f7a242db1
jakesIII <jayruma@yahoo.com>
2019-10-28 23:31:52 +0300

    add directive attribute to the html file

--- src/app/top-highlight.directive.ts
+++ src/app/top-highlight.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appTopHighlight]'
 })
 export class TopHighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
+
+  @HostListener("click") onclick(){
+    this.textDeco("rgba(31, 17, 17, 0.733)")
+  }
+
+
+  private textDeco (action:any){
+    this.elem.nativeElement.style.backgroundColor = action;
+  }
 
 }
+
+// 'rgba(31, 17, 17, 0.733)'
