b9a3846e867d23b2f27d3988df4fa6bd993565b2
jakesIII <jayruma@yahoo.com>
2019-10-28 23:13:43 +0300

    generated highlighting directive

---
+++ src/app/top-highlight.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TopHighlightDirective } from './top-highlight.directive';
+
+describe('TopHighlightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TopHighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
